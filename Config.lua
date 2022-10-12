067febf41e5cd5f142a9ff3c0ed78258d88e1305
Crazy <109050719+Crazy133@users.noreply.github.com>
2022-10-12 00:39:50 -0400

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -6,7 +6,7 @@ Config.UnZipItems = { -- Items That Can Be Used to Remove Zipties (Can Be Nothin
 
 Config.BagFallOffWait = 3 -- Time Before The Head Bag Automatically Falls Off (In Minutes)
 
-Config.ZiptieWiggleAmount = 100 -- Number of Times The Victim Must Press "Point" Before Breaking Free From Zipties
+Config.ZiptieWiggleAmount = 100 -- Number of Times The Victim Must Press "Point" Before Breaking Free From Zipties (Default: B)
 
 -- Uses Clothing Menu --
 Config.BagSelection = 49 -- Mask Selection (Default: 49)
